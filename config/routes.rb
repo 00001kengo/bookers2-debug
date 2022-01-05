fa429ee12602167f3bfa9de4f953387dbaf2930a
EC2 Default User <ec2-user@ip-172-31-1-104.ec2.internal>
2022-01-05 06:45:53 +0000

    comment commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   resources :users,only: [:show,:index,:edit,:update]
 
   resources :books do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  
+
 end
